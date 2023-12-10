688ea39cb078fe2f2a7267ca7cd80bdb1abfc61f
Abu Taher <writerabutaher@gmail.com>
2023-12-10 21:58:27 +0600

    connect db and error handling

---
+++ config/connectDB.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const connectDB = require("./config/connectDB");
+const { errorHandler, errorRouter } = require("./middleware/errorHandler");
+require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+connectDB();
+
+// use routes
 
 app.get("/", (req, res) => {
-  res.send("Welcome to my server!");
+  res.status(200).json({ message: `Server listening on port ${port}` });
 });
 
+// use error handler
+app.use(errorRouter);
+app.use(errorHandler);
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
