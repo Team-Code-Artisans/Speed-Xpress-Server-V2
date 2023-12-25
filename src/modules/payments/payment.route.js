164c775db76b680fddd6dfc57d09b9082c2d86d8
Abu Taher <writerabutaher@gmail.com>
2023-12-26 01:08:40 +0600

    Update payment.route.js

--- src/modules/payments/payment.route.js
+++ src/modules/payments/payment.route.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { createPayment } = require("./payment.controller");
 const router = express.Router();
 
-router.post("/payment", createPayment);
+router.post("/", createPayment);
 
 module.exports.PaymentRoute = router;
